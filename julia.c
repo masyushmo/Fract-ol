0bc74badb9d83d59de7e84f21ead9bb4299e9083
Maks <46930400+godfathermasei@users.noreply.github.com>
2019-01-31 12:08:36 +0200

    Add files via upload

---
+++ julia.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   julia.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmasyush <mmasyush@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/24 13:16:56 by mmasyush          #+#    #+#             */
+/*   Updated: 2019/01/24 13:16:56 by mmasyush         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fractol.h"
+
+int		julia(t_core *core, int x, int y)
+{
+	core->rx = 1.5 * (x - W_X / 2) / (0.5 * core->zoom * W_X) + core->xmove;
+	core->iy = (y - W_Y / 2) / (0.5 * core->zoom * W_Y) + core->ymove;
+	core->it = 0;
+	while (core->rx * core->rx + core->iy
+			* core->iy < 4 && core->it < core->iteration)
+	{
+		core->xx = core->rx;
+		core->yy = core->iy;
+		core->rx = core->xx * core->xx - core->iy * core->iy + core->cx;
+		core->iy = 2 * core->xx * core->yy + core->cy;
+		core->it++;
+	}
+	if (core->it == core->iteration)
+		return (0x000000);
+	else
+		return (core->color->set[core->color->c] * core->it);
+}
+
+void	set_julia(t_core *core)
+{
+	core->cx = 0;
+	core->cy = 0;
+	core->zoom = 1;
+	core->xmove = 0;
+	core->ymove = 0;
+	core->color->c = 0;
+	core->iteration = 100;
+}
---
+++ main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmasyush <mmasyush@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/17 15:06:45 by mmasyush          #+#    #+#             */
+/*   Updated: 2019/01/17 15:06:45 by mmasyush         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fractol.h"
+
+int	stop(char *str)
+{
+	ft_putstr(str);
+	exit(1);
+	return (0);
+}
+
+int	chose(char *str)
+{
+	if ((ft_strcmp("julia" , str) == 0) || \
+		(ft_strcmp("mandelbrot", str) == 0) || \
+		(ft_strcmp("mo", str) == 0))
+			return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_core	*core;
+	
+	if (argc < 2)
+		stop("ERROR: not enough args\n");
+	if (argc > 2)
+		stop("ERROR: too many args\n");
+	if (!(chose(argv[1])))
+		stop("ERROR: wrong name");
+	core = create_win(core, argv[1]);
+	fract_ol(core, argv[1]);
+	thread_add(core);
+	mlx_hook(core->win_ptr, 2, 0, key_press, core);
+	mlx_hook(core->win_ptr, 4, 0, mouse_press, core);
+	mlx_hook(core->win_ptr, 6, 0, mouse_move, core);
+	mlx_hook(core->win_ptr, 17, 0, stop, 0);
+	mlx_loop(core->mlx_ptr);
+	return (0);
+}
